66847de34a512005e8592e502ff58813a767d8fe
arsenijtelpuk8@gmail.com <arsenijtelpuk8@gmail.com>
2024-11-07 23:02:59 +0300

    небольшие баги исправлены

--- Assets/Scripts/EnemySee.cs
+++ Assets/Scripts/EnemySee.cs
@@ -7,35 +7,29 @@ public class EnemySee : MonoBehaviour
     [SerializeField] float radiysToSeeEnemy = 3.0f;
     [SerializeField] Transform playerTransform;
     [SerializeField] float timeToWait = 2.0f;
+    private Color originalColor;
+    private SpriteRenderer spriteRenderer;
 
     public void SeeEnemy()
     {
         Collider2D[] colliders = Physics2D.OverlapCircleAll(playerTransform.position ,radiysToSeeEnemy);
-        SpriteRenderer spriteRenderer;
 
         foreach (Collider2D collider in colliders)
         {
             if(collider.tag == "enemy")
             {
                 spriteRenderer = collider.GetComponent<SpriteRenderer>();
-                Color originalColor = spriteRenderer.color;
+                originalColor = spriteRenderer.color;
 
-                spriteRenderer.color = Color.Lerp(originalColor, Color.red, 0.1f);
+                spriteRenderer.color = Color.Lerp(originalColor, Color.red, 1f);
 
-                StartCoroutine(TimeToWait());
-                
-                RestoreColor(spriteRenderer, originalColor);
+                Invoke("RestoreColor", timeToWait);
             }
         }
     }
 
-    private void RestoreColor(SpriteRenderer spriteRenderer, Color originalColor)
+    private void RestoreColor()
     {
         spriteRenderer.color = originalColor;
-    }
-
-    IEnumerator TimeToWait()
-    {
-        yield return new WaitForSeconds(timeToWait);
     }
 }
